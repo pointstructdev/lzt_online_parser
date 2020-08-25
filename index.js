227e760783f0372a3b4cd263ccfedae932a2ce0c
Anton <70181590+pointstructdev@users.noreply.github.com>
2020-08-25 15:40:24 +0300

    Initialization of Puppeteer 🗿

--- index.js
+++ index.js
@@ -6,3 +6,13 @@
 
 const puppeteer = require(`puppeteer`);
 const fs = require(`fs`);
+
+(async () => {
+  // Init non-headless chromium
+
+  const browser = await puppeteer.launch({
+    args: [`--no-sandbox`],
+    headless: false,
+    ignoreHTTPSErrors: true,
+    userDataDir: `${__dirname}/profile-dir`,
+  });
